21f7bff00a75f7577625dfbcf7d037bbc1ac3f3f
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-12 01:32:14 +0900

    test

---
+++ app.js
@@ -0,0 +1,22 @@
+const { App } = require("@slack/bolt");
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true, // add this
+  appToken: process.env.SLACK_APP_TOKEN, // add this
+  port: process.env.PORT || 3000,
+});
+
+app.message("hello", async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log("⚡️ Bolt app is running!");
+})();
