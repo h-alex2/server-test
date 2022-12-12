84e561a553ca01228c244db8328fa1221cb72c6a
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-12 13:11:28 +0900

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { App } = require("@slack/bolt");
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true, // add this
+  // socketMode: true, // add this
   appToken: process.env.SLACK_APP_TOKEN, // add this
   port: process.env.PORT || 3000,
 });
