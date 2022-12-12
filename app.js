bf2a2ca513cdeeb6adc99b3e4ec79b99fb7e9ccb
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-12 22:37:37 +0900

    test

--- app.js
+++ app.js
@@ -20,16 +20,17 @@ const expressApp = expressReceiver.app;
 const { WebClient } = require("@slack/web-api");
 app.client = new WebClient(process.env.SLACK_APP_TOKEN);
 
-expressReceiver.router.post('/', (req, res) => {
-  const payload = JSON.parse(req.body.payload)
+expressReceiver.router.post("/", (req, res) => {
+  const payload = JSON.parse(req.body.payload);
   res.send(req.data);
 });
 
-expressReceiver.router.post('/slack/events', (req, res) => {
+expressReceiver.router.post("/slack/events", (req, res) => {
   res.send(req.data);
 });
 
-expressReceiver.router.post('/actions', (req, res) => {
+expressReceiver.router.post("/slack/actions", (req, res) => {
+  console.log("hihihiiihihihi");
   res.send(req.data);
 });
 
